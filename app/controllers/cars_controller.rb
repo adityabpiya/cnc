0a636138f4b992d8f2807e24cdfa96e34372540f
adityabpiya <aditya.b.piya@gmail.com>
2019-08-01 19:58:37 +0000

    form accepts info and saves it

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -7,4 +7,18 @@ class CarsController < ApplicationController
     def new
         @car = Car.new
     end
+
+    def create
+        Car.create(car_params)
+        redirect_to root_path
+    end
+
+
+    private
+
+    def car_params
+        params.require(:car).permit(:year, :make, :model, :mods, :college, :major)
+    end
+
+
 end
